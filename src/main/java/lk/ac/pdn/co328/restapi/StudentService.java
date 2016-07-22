54d799a36a4da83eef02c86dc9c789e6d39a4e17
Danoja Dias <e12061@ce.pdn.ac.lk>
2016-07-22 19:44:34 +0530

    PUT and DELETE requests...

--- src/main/java/lk/ac/pdn/co328/restapi/StudentService.java
+++ src/main/java/lk/ac/pdn/co328/restapi/StudentService.java
@@ -8,11 +8,13 @@ import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import lk.ac.pdn.co328.studentSystem.Student;
+import lk.ac.pdn.co328.studentSystem.StudentRegister;
 import org.jboss.resteasy.util.HttpResponseCodes;
  
 @Path("rest")
 public class StudentService
-{    
+{
+    StudentRegister sr=new StudentRegister();
     @GET
     @Path("student/{id}")
     // Uncommenting this will let the reciver know that you are sending a json
@@ -35,8 +37,15 @@ public class StudentService
     @Path("student/{id}")
     public Response deleteStudent(@PathParam("id") int id)
     {
-        String message = "{message:'FIXME : Delete service is not yet implemented'}";  // Ideally this should be machine readable format Json or XML 
-        return Response.status(HttpResponseCodes.SC_OK).entity(message).build();
+
+        Student st = sr.findStudent(id);
+
+        String message = "Student id " +id+" is not found";  // Ideally this should be machine readable format Json or XML
+
+
+        if(st==null) return Response.status(HttpResponseCodes.SC_BAD_REQUEST).entity(message).build();
+        sr.removeStudent(id);
+        return Response.status(HttpResponseCodes.SC_OK).entity("Student id " +id+" is deleted.").build();
     }
     
     @PUT
@@ -44,8 +53,25 @@ public class StudentService
     @Consumes("application/xml")
     public Response addStudent(@PathParam("id") int id, String input)
     {
-        String message = "{message:'FIXME : Add service is not yet implemented'}";  // Ideally this should be machine readable format Json or XML 
-        return Response.status(HttpResponseCodes.SC_OK).entity(message).build();
+
+        Student alreadyRegistered=sr.findStudent(id);
+        if(alreadyRegistered!=null) {
+            return Response.status(HttpResponseCodes.SC_BAD_REQUEST).entity("This student id " + id
+                    + " is already added").build();
+
+        }
+        String ary[]=input.split("\\s+");;
+
+        Student st =new Student(id,ary[0],ary[1]);
+
+        try {
+            sr.addStudent(st);
+            return Response.status(HttpResponseCodes.SC_OK).entity("Student id "+id+" is added").build();
+        } catch (Exception e) {
+            return Response.status(HttpResponseCodes.SC_BAD_REQUEST).entity(e).build();
+        }
+
+
     }
 }
 
