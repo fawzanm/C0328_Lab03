1f66157d9718d94afeffc16b805ca4b280f23bf4
aaa <kampriyadarshani00000@gmail.com>
2016-07-25 01:04:06 +0530

    veiw student and modify student are done

--- src/main/java/lk/ac/pdn/co328/restapi/StudentService.java
+++ src/main/java/lk/ac/pdn/co328/restapi/StudentService.java
@@ -8,29 +8,63 @@ import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import lk.ac.pdn.co328.studentSystem.Student;
+import lk.ac.pdn.co328.studentSystem.StudentRegister;
 import org.jboss.resteasy.util.HttpResponseCodes;
- 
+
 @Path("rest")
 public class StudentService
-{    
+{
+
+    private StudentRegister register;
+
+    public StudentService()
+    {
+        register = new StudentRegister();
+    }
+
+
     @GET
     @Path("student/{id}")
     // Uncommenting this will let the reciver know that you are sending a json
     @Produces( MediaType.APPLICATION_JSON + "," + MediaType.APPLICATION_XML )
+
     public Response viewStudent(@PathParam("id") int id) {
-        Student st = new Student(id, "dummy", "dummy");
+
+        // Student st = new Student(id, "dummy", "dummy");
+        Student st = register.findStudent(id);
+        if(st==null)
+            return Response.status(HttpResponseCodes.SC_NOT_FOUND).entity("Error : Student not found").build();
         return Response.status(HttpResponseCodes.SC_FOUND).entity(st).build();
+
+
     }
-    
+
     @POST
     @Path("student/{id}")
     @Consumes("application/xml")
     public Response modifyStudent(@PathParam("id") int id, String input)
     {
-        String message = "{message:'FIXME : Update service is not yet implemented'}";  // Ideally this should be machine readable format Json or XML 
+        //String message = "{message:'FIXME : Update service is not yet implemented'}";  // Ideally this should be machine readable format Json or XML
+        // return Response.status(HttpResponseCodes.SC_OK).entity(message).build();
+        Student st = register.findStudent(id);
+        if(st==null)
+            return Response.status(HttpResponseCodes.SC_NOT_FOUND).entity("Error : Student with ID " + id + " not found").build();
+        register.removeStudent(id);
+        String message = "";
+        try {
+            String s[] = input.split("\r\n");
+            String names[] = s[3].split(",");
+            Student std = new Student(id, names[0], names[1]);
+            register.addStudent(std);
+            message = "Success : Student with ID " + id + " updated";
+        } catch (Exception e) {
+            message = e.toString();
+            return Response.status(HttpResponseCodes.SC_BAD_REQUEST).entity(message).build();
+        }
         return Response.status(HttpResponseCodes.SC_OK).entity(message).build();
     }
-    
+
+
     @DELETE
     @Path("student/{id}")
     public Response deleteStudent(@PathParam("id") int id)
--- src/main/java/lk/ac/pdn/co328/studentSystem/StudentRegister.java
+++ src/main/java/lk/ac/pdn/co328/studentSystem/StudentRegister.java
@@ -1,6 +1,8 @@
 package lk.ac.pdn.co328.studentSystem;
 import java.util.ArrayList;
 
+import java.util.ArrayList;
+
 public class StudentRegister
 {
     private ArrayList<Student> studentList = new ArrayList<Student>();
@@ -42,10 +44,21 @@ public class StudentRegister
         return  null;
     }
 
+
+
+    //Checking the registry list is empty or not
+    public boolean isEmpty(){
+        if (studentList.isEmpty()){
+            return true;
+        }else {
+            return false;
+        }
+    }
+
     // Cleans all the data from the student register
     public void reset()
     {
-        studentList = null;
+        studentList.clear();
     }
 
     // Finds all the students that has the given name as a part of their name.
@@ -56,13 +69,13 @@ public class StudentRegister
         {
             if(studentList.get(i).getFirstName().contains(name))
             {
-                studentList.add(studentList.get(i));
+                students.add(studentList.get(i));
             }
-			
-			
+
+
             if(studentList.get(i).getLastName().contains(name))
             {
-                studentList.add(studentList.get(i));
+                students.add(studentList.get(i));
             }
         }
         return students;
