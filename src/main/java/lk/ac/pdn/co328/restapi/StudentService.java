4b7a2ff8904f10342acf795356f8f2b57b7adb1b
gunarathnemdd <gunarathnemdd@gmail.com>
2016-08-01 10:03:01 +0530

    finished the lab

--- src/main/java/lk/ac/pdn/co328/restapi/StudentService.java
+++ src/main/java/lk/ac/pdn/co328/restapi/StudentService.java
@@ -13,21 +13,43 @@ import org.jboss.resteasy.util.HttpResponseCodes;
 @Path("rest")
 public class StudentService
 {    
+    StudentRegister stRegister = new StudentRegister();
     @GET
     @Path("student/{id}")
     // Uncommenting this will let the reciver know that you are sending a json
-    @Produces( MediaType.APPLICATION_JSON + "," + MediaType.APPLICATION_XML )
+    @Produces( MediaType.APPLICATION_XML )
     public Response viewStudent(@PathParam("id") int id) {
-        Student st = new Student(id, "dummy", "dummy");
-        return Response.status(HttpResponseCodes.SC_FOUND).entity(st).build();
+        String message;
+        Student st = stRegister.findStudent(id);
+        if(st == null){
+            message = "The ID " + id + " is not found";
+            return Response.status(HttpResponseCodes.SC_NOT_FOUND).entity(message).build();
+        }else{
+            return Response.status(HttpResponseCodes.SC_FOUND).entity(st).build();
+        }
+
     }
     
     @POST
     @Path("student/{id}")
     @Consumes("application/xml")
-    public Response modifyStudent(@PathParam("id") int id, String input)
+    public Response modifyStudent(@PathParam("id") int id, Student input)
     {
-        String message = "{message:'FIXME : Update service is not yet implemented'}";  // Ideally this should be machine readable format Json or XML 
+        String message; // Ideally this should be machine readable format Json or XML
+        Student st = stRegister.findStudent(id);
+        if(st == null){
+            message = "There is no student to update in the database";
+            return Response.status(HttpResponseCodes.SC_NOT_FOUND).entity(message).build();
+        }else synchronized (stRegister) {
+            try {
+                stRegister.removeStudent(id);
+                stRegister.addStudent(input);
+                message = "Student updated Successfully";
+            } catch (Exception e) {
+                message = e.getMessage();
+                return Response.status(HttpResponseCodes.SC_BAD_REQUEST).entity(message).build();
+            }
+        }
         return Response.status(HttpResponseCodes.SC_OK).entity(message).build();
     }
     
@@ -35,16 +57,35 @@ public class StudentService
     @Path("student/{id}")
     public Response deleteStudent(@PathParam("id") int id)
     {
-        String message = "{message:'FIXME : Delete service is not yet implemented'}";  // Ideally this should be machine readable format Json or XML 
+        String message;// Ideally this should be machine readable format Json or XML
+        Student st = stRegister.findStudent(id);
+        if(st == null){
+            message = "There is no student with this ID!";
+            return Response.status(HttpResponseCodes.SC_NOT_FOUND).entity(message).build();
+        }else synchronized (stRegister) {
+                stRegister.removeStudent(id);
+                message = "Student with ID = " + id + " was removed from the database.";
+        }
         return Response.status(HttpResponseCodes.SC_OK).entity(message).build();
     }
     
     @PUT
     @Path("student/{id}")
     @Consumes("application/xml")
-    public Response addStudent(@PathParam("id") int id, String input)
+    public Response addStudent(@PathParam("id") int id, Student st)
     {
-        String message = "{message:'FIXME : Add service is not yet implemented'}";  // Ideally this should be machine readable format Json or XML 
+        String message;
+        // Ideally this should be machine readable format Json or XML
+        Student isStudent = stRegister.findStudent(id);
+        synchronized (stRegister) {
+            try {
+                stRegister.addStudent(st);
+                message = "Student added Successfully";
+            } catch (Exception e) {
+                message = e.toString();
+                return Response.status(HttpResponseCodes.SC_BAD_REQUEST).entity(message).build();
+            }
+        }
         return Response.status(HttpResponseCodes.SC_OK).entity(message).build();
     }
 }
